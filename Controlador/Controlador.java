566a82ae2beaa1da8cb8cb0cbd1ee1d900ede554
Roberstxx07 <132015061+Roberstxx07@users.noreply.github.com>
2024-10-17 11:53:51 -0600

    Delete Controlador directory

--- Controlador/Controlador.java
+++
@@ -1,7 +0,0 @@
-public class Controlador {
-    public static void main(String[] args) {
-        // Ejecuta Reloj y Ejecutor en hilos separados
-        new Thread(new Reloj()).start();
-        new Thread(new Ejecutor("calc", 0)).start();
-    }
-}
